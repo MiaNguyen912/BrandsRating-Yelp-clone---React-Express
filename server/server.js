8100b7cc98cc34d846de7524d26dad10e1a995bc
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-04-18 01:42:59 -0700

    add post method to post new review in server.js

--- server/server.js
+++ server/server.js
@@ -77,10 +77,18 @@ app.post("/api/v1/brandRatings", async (req,res)=>{
                 brand: results.rows[0]
             }
         })
-    } catch (err){
-        console.log(err);
-    }
-    
+    } catch (err){console.log(err);}
+})
+
+//create new review for a brand
+app.post("/api/v1/brandRatings/:id/addReview", async(req, res) => {
+    try{
+        const newReview = await db.query("INSERT INTO reviews (brand_id, name, review, rating) VALUES ($1,$2,$3,$4) RETURNING *;", [req.params.id, req.body.name, req.body.review, req.body.rating])
+        res.status(201).json({
+            status: 'success',
+            data:{review: newReview.rows[0]}
+         })
+    } catch(err){console.log(err)}
 })
 
 //update brands
